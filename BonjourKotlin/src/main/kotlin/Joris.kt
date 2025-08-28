a3e299cd1e44488294677ef31a8d14a77b9ba7cb
Naïm Guénette-Harvey <2360188@cegepmontpetit.ca>
2025-08-28 09:09:29 -0400

    Finalisation de l'exercice Kotlin

--- BonjourKotlin/src/main/kotlin/Joris.kt
+++ BonjourKotlin/src/main/kotlin/Joris.kt
@@ -1,12 +1,9 @@
 package org.GuenetteHarvey
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val name = "Joris"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Bonjour, " + name)
+
+    println("Bonjour," + name)
 
 
 }
